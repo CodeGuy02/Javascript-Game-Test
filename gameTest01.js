77a8e9cc0775188c299902604f903e2703cf270c
codeguy02 <codeguy02@example.com>
2021-06-27 21:20:55 -0400

    loading graphics

--- gameTest01.js
+++ gameTest01.js
@@ -1,21 +1,49 @@
 
 function first() {
 
+playField = document.getElementById("playField");
+controlField = document.getElementById("controlField");
+
+w = playField.clientWidth;
+l = playField.clientHeight;
+controlField.innerHTML = "Width: " + w + " Length: " + l;
+
+playFieldCanvas = document.getElementById("playFieldCanvas");
+
 // initialize kaboom context
-const k = kaboom();
+// const k = kaboom();
+
+const k = kaboom({
+    width:w,
+    height:l,
+    canvas:playFieldCanvas,
+});
 
 // define a scene
 k.scene("main", () => {
 
     // draw background on the bottom, ui on top, layer "obj" is default
-    layers([
+    k.layers([
         "bg",
         "obj",
         "ui",
     ], "obj");
 
+    //const sky = k.add([
+    //    k.sprite("sky", "sky.png"),
+    //]);
+
+    //k.loadRoot("https://github.com/CodeGuy02/Javascript-Game-Test/blob/364b4279d67834c2ee19f192259d7f4afe0b3fbe/");
+    //k.loadRoot("https://cors-anywhere.herokuapp.com/https://github.com/CodeGuy02/Javascript-Game-Test/tree/364b4279d67834c2ee19f192259d7f4afe0b3fbe/mountains/");
+    //k.loadRoot("https://github.com/CodeGuy02/Javascript-Game-Test/tree/364b4279d67834c2ee19f192259d7f4afe0b3fbe/mountains/");
+    //k.loadRoot("https://game-project-demo-01.s3.us-east.cloud-object-storage.appdomain.cloud/");
+    k.loadRoot("file:///mountains/")
+    k.loadSprite('sky', 'background_glacial_mountains.png');
+
     // add a text at position (100, 100)
     k.add([
+        k.sprite("sky"),
+        k.layer("bg"),
         k.text("READY", 32),
         k.pos(100, 100),
     ]);
